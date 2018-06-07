5300bf211658c5218468ca5345f1a6fb834c864f
urbaniak.michal@yahoo.com <urbaniak.michal@yahoo.com>
2018-06-07 20:46:48 +0200

    Initial commit

---
+++ src/main/java/com/motoPeople/motoPeople/MotoPeopleApplication.java
@@ -0,0 +1,12 @@
+package com.motoPeople.motoPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoPeopleApplication.class, args);
+	}
+}
